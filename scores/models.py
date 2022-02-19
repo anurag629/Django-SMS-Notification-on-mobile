dd04b2c354afc6971b21dfdb420f9167f9b8fe4f
Your Name <you@example.com>
2022-02-19 09:52:01 +0530

    creted save method in models

--- scores/models.py
+++ scores/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from twilio.rest import Client
 
 # Create your models here.
 
@@ -8,3 +9,17 @@ class Score(models.Model):
 
     def __str__(self):
         return str(self.result)
+
+    def save(self, *args, **kwargs):
+        if self.result < 70:
+            account_sid = 'ACbf1f7d728696d08a18ba347b8abf5906'
+            auth_token = '5c08aabd9cf5f3c75e1d26095ee433bc'
+            client = Client(account_sid, auth_token)
+
+            message = client.message.create(
+                body=f'The current result is bad - {self.result}',
+                from_='+19036622331',
+                to='+918052027789'
+            )
+            print(message.sid)
+        return super().save(*args, **kwargs)
